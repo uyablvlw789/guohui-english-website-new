d5163dc589356ca0f3c7f9e8b16651568c927436
uyablvlw789 <ciel2018@gmail.com>
2024-01-19 17:15:33 +0800

    change ImageCarousel

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -9,7 +9,7 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Component({
   imageList = ["/hero1.jpg", "/hero2.jpg", "/hero3.jpg", "/hero4.jpg"],
@@ -31,7 +31,7 @@ export default function Component({
     >
       {imageList.map((imgUrl) => (
         <SwiperSlide key={imgUrl}>
-          <img src={imgUrl} alt={imgUrl} />
+          <Image src={imgUrl} alt={imgUrl} fill objectFit="cover" />
         </SwiperSlide>
       ))}
     </Swiper>
